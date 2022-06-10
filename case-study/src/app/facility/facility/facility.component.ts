a354edcc7f6474f32adad77ae6ddc5055ac68448
nghiatrinh107 <nghiatrinh107@gmail.com>
2022-06-10 17:02:06 +0700

    ss5

--- case-study/src/app/facility/facility/facility.component.ts
+++ case-study/src/app/facility/facility/facility.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-facility',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./facility.component.css']
 })
 export class FacilityComponent implements OnInit {
+  private scripUrls = [
+    'assets/js/jquery.min.js',
+    'assets/js/popper.min.js',
+    'assets/js/bootstrap.min.js',
+    'assets/js/roberto.bundle.js',
+    'assets/js/default-assets/active.js',
+    'assets/js/default-assets/card.js'
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.loadScrip();
+  }
+
+  public loadScrip() {
+    for (const scripUrl of this.scripUrls) {
+      const body = document.body as HTMLDivElement;
+      const scrip = document.createElement('script');
+      scrip.innerHTML = '';
+      scrip.src = scripUrl;
+      body.appendChild(scrip);
+    }
+  }
 }
